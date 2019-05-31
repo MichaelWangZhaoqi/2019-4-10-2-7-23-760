53a19354ec37783cc8c68b23e6cdd60d8b6bdfb3
MichaelWangZhaoqi <50286442+MichaelWangZhaoqi@users.noreply.github.com>
2019-05-31 10:49:31 +0800

    First Version

--- main/main.js
+++ main/main.js
@@ -1,4 +1,19 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+    var dis=inputs['distance'];
+	var park=inputs['parkTime'];
+	var price;
+	
+	if(dis<=2){
+		price=6+0.25*park;
+	}
+	
+	else if (dis<=8){
+		price=6+(dis-2)*0.8+0.25*park;
+	}
+	
+	else
+		price=6+4.8+(dis-8)*1.2+0.25*park;
+	
+	
+    return Math.round(price);
 };
